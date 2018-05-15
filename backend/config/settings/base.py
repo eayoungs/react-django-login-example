74aaee9f0a5b7d8beba4ea10891a93a56e5c4537
Borislav Hadzhiev <opmftw@abv.bg>
2018-05-15 21:07:42 +0300

    Update base.py

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -2,7 +2,7 @@ import os
 import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SECRET_KEY = '$5pan&)k_caa*a=y_i&4zoz)9gzyl4h=-9g%3mc+g6-au*g&ak'
 
 INSTALLED_APPS = [
